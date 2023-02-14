5381563f9d3883efbf3185de8e2247c34cf37615
David Profitt <dlprofitt@gmail.com>
2023-02-14 14:50:10 -0500

    first commit

---
+++ arrays-intro/main.js
@@ -0,0 +1,20 @@
+const fruits = ["Banana", "Orange", "Apple", "Watermelon", "Blueberry"];
+
+console.log(fruits);
+
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+];
+
+const glass1 = dishes[1];
+const glass2 = dishes[4];
+const glass3 = dishes[9];
+
+console.log("I am cleaning the following glasses:");
+
+console.log(glass1);
+console.log(glass2);
+console.log(glass3);
---
+++ variables/main.js
@@ -0,0 +1,14 @@
+let firstName = 'David';
+let lastName = 'Profitt';
+let age = 36;
+let likesTravel = true;
+
+console.log(firstName, lastName, age, likesTravel);
+
+let temperature = 61;
+let conditions = 'Sunny';
+let sunriseTime = '7:21AM';
+let sunsetTime = '6:05PM';
+let todaysWeather = `It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+
+console.log(todaysWeather);
